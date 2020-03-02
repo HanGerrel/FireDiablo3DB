0b0bfcaba511049e3e42a694f302abbad98f95cf
HanGerrel <volodimir.grigoryev@gmail.com>
2020-03-02 08:30:06 +0200

    Code formatting

--- diabloapi.py
+++ diabloapi.py
@@ -16,22 +16,22 @@ def get_token():
 
 
 def season_leaderboard(token, season=19):
-    url = 'https://eu.api.blizzard.com/data/d3/season/' + str(season) + token
+    url = "https://eu.api.blizzard.com/data/d3/season/{}{}".format(str(season), token)
     return url
 
 
 def era_leaderboard(token, hero_class='barbarian', era=12):
-    url = "https://eu.api.blizzard.com/data/d3/era/" + str(era) + "/leaderboard/rift-" + hero_class + token
+    url = "https://eu.api.blizzard.com/data/d3/era/{}/leaderboard/rift-{}{}".format(str(era), hero_class, token)
     return url
 
 
 def season_index(token):
-    url = 'https://eu.api.blizzard.com/data/d3/season/' + token
+    url = 'https://eu.api.blizzard.com/data/d3/season/{}'.format(token)
     return url
 
 
 def era_index(token):
-    url = 'https://us.api.blizzard.com/data/d3/era/' + token
+    url = 'https://us.api.blizzard.com/data/d3/era/{}'.format(token)
     return url
 
 
@@ -43,16 +43,16 @@ def get_battle_tag(battle_tag):
 
 def profile(token, battle_tag):
     battle_tag = get_battle_tag(battle_tag)
-    url = 'https://eu.api.blizzard.com/d3/profile/' + battle_tag + '/' + token
+    url = 'https://eu.api.blizzard.com/d3/profile/{}/{}'.format(battle_tag, token)
     return url
 
 
 def hero(token, battle_tag, hero_id):
     battle_tag = get_battle_tag(battle_tag)
-    url = 'https://eu.api.blizzard.com/d3/profile/' + battle_tag + '/hero/' + str(hero_id) + token
+    url = 'https://eu.api.blizzard.com/d3/profile/{}/hero/{}{}'.format(battle_tag, str(hero_id), token)
     return url
 
 
 def get_act(token):
-    url = 'https://eu.api.blizzard.com/d3/data/act' + token
+    url = 'https://eu.api.blizzard.com/d3/data/act{}'.format(token)
     return url
